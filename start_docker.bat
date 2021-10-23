e4d92dec17b0ee83f5bba7b849b94acd5090a346
Nikita Koschin <nik.koshin@mail.ru>
2021-10-23 19:16:45 +0300

    fix start_docker bat

--- start_docker.bat
+++ start_docker.bat
@@ -1,2 +1,2 @@
 gradlew build && docker build --build-arg JAR_FILE=build/libs/server-1.0.jar -t springio/gs-spring-boot-docker . && docker run -p 8080:8080 springio/gs-spring-boot-docker
-pause
+cmd
